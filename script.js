a92a15d401ea909577d5fdd34b784ca8480e6d57
Trent Townsend <trent.townsend2@gmail.com>
2023-01-21 00:46:49 +1000

    Attach function to event listener to open/close accordions and transitions

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const questionArray = document.querySelectorAll('.question');
 
 questionArray.forEach(question => {
     question.addEventListener("click", () => {
-        question.classList.add("open");
-        question.nextElementSibling.classList.remove('hidden')
+
+        if(question.classList.contains('open')){
+            question.classList.remove('open');
+            question.nextElementSibling.classList.add('hidden');
+        }
+        else {
+            question.classList.add("open");
+            question.nextElementSibling.classList.remove('hidden')
+        }
+
      })
 });
