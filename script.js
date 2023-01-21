855ca984d0001021aa022d3657a9585549cf8bce
Trent Townsend <trent.townsend2@gmail.com>
2023-01-21 22:25:22 +1000

    Adjust ease of toggling for better UX. Add transition to toggle arrows and position background svg

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const questionArray = document.querySelectorAll('.question');
+const questionArray = document.querySelectorAll(".question");
 
-questionArray.forEach(question => {
-    question.addEventListener("click", () => {
-
-        if(question.classList.contains('open')){
-            question.classList.remove('open');
-            question.nextElementSibling.classList.add('hidden');
-        }
-        else {
-            question.classList.add("open");
-            question.nextElementSibling.classList.remove('hidden')
-        }
-
-     })
+questionArray.forEach((question) => {
+  question.addEventListener("click", () => {
+    if (question.classList.contains("open")) {
+      question.classList.remove("open");
+      question.nextElementSibling.classList.add("hidden");
+    } else {
+      question.classList.add("open");
+      question.nextElementSibling.classList.remove("hidden");
+    }
+  });
 });
